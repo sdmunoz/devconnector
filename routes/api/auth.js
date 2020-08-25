25ed5185a36bd99c14fcf8280229ec0551af9032
Sammy Munoz <sdmunoz@yahoo.com>
2020-08-24 21:22:34 -0500

    Added Auth Middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   Get api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth Route'));
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    return res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
