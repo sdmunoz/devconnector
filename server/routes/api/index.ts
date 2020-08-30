3ef163a581e70a4b01549daea23baf751185ba38
Sammy Munoz <sdmunoz@yahoo.com>
2020-08-30 10:12:19 -0500

    Added Typedef

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -1,17 +1,17 @@
-import * as express from 'express';
+import express, { Router, Request, Response } from 'express';
 import auth from './auth/';
 import posts from './posts';
 import profile from './profile';
 import users from './users/post-register-user';
 
-const router = express.Router();
+const router: Router = express.Router();
 
 router.use('/api/auth', auth);
 router.use('/api/posts', posts);
 router.use('/api/profile', profile);
 router.use('/api/users', users);
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Connected!' });
 });
 
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import connectDB from '../../devconnector/config/db.js';
 import mainRoutes from './routes/api/index';
 
-const app = express();
+const app: Application = express();
 
 // connect Database
 connectDB();
@@ -10,11 +10,11 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req: Request, res: Response) => res.send('API Running'));
 
 // Define Main Route
 app.use('/', mainRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT: number | string = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
