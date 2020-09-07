1938f63d84ed72f02773c1a0448f8fe9671df117
Sammy Munoz <sdmunoz@yahoo.com>
2020-09-07 09:03:39 -0500

    Test Post users API

--- cypress/integration/api/users/users.spec.js
+++ cypress/integration/api/users/users.spec.js
@@ -4,6 +4,35 @@ describe('Users API endpoints', () => {
   const email = 'testuser02@yahoo.com';
   const password = 'password';
 
+  before(() => {
+    // Login to Test user
+    cy.request({
+      method: 'POST',
+      url: '/api/auth',
+      body: { email, password },
+    })
+      .its('body')
+      .then((response) => {
+        token = response.token;
+        expect(token).to.not.equal('');
+        cy.log('Test user has logged in.');
+      });
+  });
+
+  beforeEach(() => {
+    // Delete existing Test user
+    cy.request({
+      method: 'DELETE',
+      url: '/api/profile',
+      headers: { 'x-auth-token': token },
+    })
+      .its('body')
+      .then((response) => {
+        expect(response.msg).to.equal('User Deleted');
+        cy.log('Existing User has been deleted.');
+      });
+  });
+
   it('should test user registration', () => {
     cy.request({
       method: 'POST',
@@ -14,7 +43,6 @@ describe('Users API endpoints', () => {
       .then((response) => {
         token = response.token;
         expect(token).to.not.equal('');
-        cy.log('Token:', token);
         cy.log('User has been created.');
       });
   });
