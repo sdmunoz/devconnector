f664b286dc3b3e35dfb4ad122f4c9ffa8f8a67fa
Sammy Munoz <sdmunoz@yahoo.com>
2020-09-13 14:16:39 -0500

    Added react-intl

--- client/src/shared/components/landing/landing.component.tsx
+++ client/src/shared/components/landing/landing.component.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 
 const Landing = (props: any) => {
-  //   const classes = styles();
+  const classes = styles();
   return (
     <Container>
-      <Typography variant='h2' gutterBottom align='center'>
-        Devconnector
-      </Typography>
-      <Button color='primary' variant='contained'>
-        Signup
-      </Button>
-      <Button color='secondary' variant='contained'>
-        Login
-      </Button>
+      <Grid item className={classes.gridCenter}>
+        <Typography variant='h2' gutterBottom align='center'>
+          Devconnector
+        </Typography>
+      </Grid>
+      <Grid item className={classes.gridCenter}>
+        <Button
+          color='primary'
+          variant='contained'
+          className={classes.buttonMargin}
+        >
+          Signup
+        </Button>
+        <Button color='secondary' variant='contained'>
+          Login
+        </Button>
+      </Grid>
     </Container>
   );
 };
 
-// const styles = makeStyles(() => ({}));
+const styles = makeStyles(() => ({
+  gridCenter: {
+    textAlign: 'center',
+  },
+  buttonMargin: {
+    marginRight: 10,
+  },
+}));
 
 export default Landing;
